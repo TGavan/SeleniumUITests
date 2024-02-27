73371dc12c51a8f9fe1541e9fe276c739319b1ba
Gav <tyagigav@gmail.com>
2024-02-27 23:36:26 +0100

    committing a new comment

--- src/test/java/com/vertical/webtests/pageobjects/DynamicLoadedPage.java
+++ src/test/java/com/vertical/webtests/pageobjects/DynamicLoadedPage.java
@@ -33,6 +33,7 @@ public class DynamicLoadedPage {
     }
 
     public DynamicLoadedPage waitForHelloWorldTextToBeDisplayed() {
+        //Dynamic wait for the element
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.visibilityOf(helloWorldElement));
         return new DynamicLoadedPage(driver);
